7383676187b118f7778501b986268bcb021b52fe
hvilleda85 <84430169+hvilleda85@users.noreply.github.com>
2021-05-20 15:53:13 -0700

    Added organ buttons to project

--- Assets/ARXray/Scripts/OrganList.cs
+++ Assets/ARXray/Scripts/OrganList.cs
@@ -6,9 +6,51 @@ using UnityEngine.UI;
 public class OrganList : MonoBehaviour
 {
     public Image brainIcon;
+    public Image heartIcon;
+    public Image rightKidneyIcon;
+    public Image leftKidneyIcon;
+    public Image stomachIcon;
+    public Image rightLungIcon;
+    public Image leftLungIcon;
+    public Image bladderIcon;
 
     public void OnBrainFound()
     {
         brainIcon.color = Color.white;
+    }
+
+    public void OnHeartFound()
+    {
+        heartIcon.color = Color.white;
+    }
+
+    public void OnRightKidneyFound()
+    {
+        rightKidneyIcon.color = Color.white;
+    }
+
+    public void OnLeftKidneyFound()
+    {
+        leftKidneyIcon.color = Color.white;
+    }
+
+    public void OnStomachFound()
+    {
+        stomachIcon.color = Color.white;
+    }
+
+    public void OnRightLungFound()
+    {
+        rightLungIcon.color = Color.white;
+    }
+
+    public void OnLeftLungFound()
+    {
+        leftLungIcon.color = Color.white;
+    }
+
+    public void OnBladderFound()
+    {
+        bladderIcon.color = Color.white;
     }
 }
